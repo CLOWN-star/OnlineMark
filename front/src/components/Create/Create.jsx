3356754ddf6000cf24e84d74c3641b078ff78e54
-CLOWN <2766838022@qq.com>
2021-12-12 16:55:14 +0800

    Optimize the image upload interface
    Without backend

--- front/src/components/Create/Create.jsx
+++ front/src/components/Create/Create.jsx
@@ -1,4 +1,4 @@
-import React , { useState }from 'react';
+import React , { useState,useRef }from 'react';
 import { TextField } from '@material-ui/core';
 import { useStyles } from "./styles.js";
 import DropZone from "../../components/DropZone";
@@ -11,26 +11,89 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import Card  from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 
-var name;
-var desc;
+
 
 const Create = () => {
+    let fileurl =[];
+    let select = -1;
+    let filenumber = 0
     const classes = useStyles();
-    const [selectedFile, setSelectedFile] = useState();
+
+
     const [desc, setDesc] = useState()
-
-
-   function  createTask(event){  
-        event.preventDefault();
-        var file1 = localStorage.getItem("file1");
-        var file2 = localStorage.getItem("file2");
-        var file3 = localStorage.getItem("file3");
-        console.log(desc);
+    
+    function  SelectedFile(event){  
+        select = filenumber;
+        filenumber++;
+        console.log("url = "+event)
+        console.log("s"+select+"f"+filenumber)
+        fileurl.push(event);   
     }
 
+    function  GetSlelctedFile(event){  
+        sessionStorage.setItem("chooseuploadimg",fileurl[select]);
+    }
 
-    
-    
+    function  Back(event){  
+        
+        if(select==0||select==-1){
+            alert("已经是第一张")
+        }
+        else{
+            select = select - 1;
+            sessionStorage.setItem("chooseuploadimg",fileurl[select]);
+        }
+        console.log("s"+select+"f"+filenumber)
+    }
+
+    function  Next(event){  
+        
+        if(select==filenumber||select==-1){
+            alert("请上传图片")
+        }
+        else if(select==filenumber-1){
+            select++;
+            sessionStorage.setItem("chooseuploadimg","-1");
+        }
+        else{
+            select++;
+            sessionStorage.setItem("chooseuploadimg",fileurl[select]);
+        }
+        console.log("s"+select+"f"+filenumber)
+    }
+
+    function  Delete(event){  
+        if(filenumber==1){
+            fileurl.pop();
+            select = -1;
+            filenumber = 0;
+            sessionStorage.setItem("chooseuploadimg","-1");
+        }
+        else if(select==filenumber-1&&select!=0){
+            fileurl.pop();
+            select = select-1;
+            filenumber--;
+            sessionStorage.setItem("chooseuploadimg",fileurl[select]);
+        }
+        else{
+            console.log("chufa")
+            let tmp = fileurl;
+            for(let i = select;i <tmp.length-1;i++){
+                tmp[i] = tmp[i+1]
+            }
+            tmp.pop();
+            console.log(tmp)
+            fileurl = tmp;
+            filenumber--;
+            sessionStorage.setItem("chooseuploadimg",fileurl[select]);
+        }
+        console.log("s"+select+"f"+filenumber)
+    }
+
+    function  createTask(event){  
+        console.log(desc)
+    }
+
     return (
         <div className={classes.page}>
             <form>
@@ -42,7 +105,7 @@ const Create = () => {
                 </div>
                 <div className={classes.content}>
                     <div className={classes.dropzone}>
-                         <DropZone onFileUploaded={setSelectedFile} />
+                         <DropZone onFileUploaded={SelectedFile} GetselectFile={GetSlelctedFile} Back={Back} Next={Next} Delete={Delete}/>
                     </div>
                     <fieldset>
                         <Card sx={{ maxWidth: 300 }}>
@@ -73,4 +136,3 @@ const Create = () => {
     };
 
 export default Create;
-
--- front/src/components/DropZone/index.js
+++ front/src/components/DropZone/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback ,useRef} from 'react';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import {useDropzone} from 'react-dropzone';
 import { useStyles } from "./styles.js";
@@ -9,11 +9,12 @@ import { AddLink } from '@mui/icons-material';
 
 let buffer;
 
-const DropZone = ({ onFileUploaded })  => {
+const DropZone = ({ onFileUploaded ,GetselectFile, Back, Next, Delete})  => {
   const classes = useStyles();
-  const [showFile, setShowFile] = useState('');
-  const [ Count, SetCount ] = useState(1)
-  let addp
+  const [showFile, setShowFile] = useState("-1");
+  const showFileRef = useRef(showFile);
+  showFileRef.current = showFile;
+
   const ipfs = create({
     host: "ipfs.infura.io",
     port: 5001,
@@ -23,89 +24,16 @@ const DropZone = ({ onFileUploaded })  => {
   
   async function gen(){
       let result = await ipfs.add(buffer);  
-      console.log("before");
-      var file1 = localStorage.getItem("file1");
-      var file2 = localStorage.getItem("file2");
-      var file3 = localStorage.getItem("file3");  
-      console.log(file1);
-      console.log(file2);
-      console.log(file3);  
-      let URI = `https://ipfs.infura.io/ipfs/${result.path}`;    
-       alert("上传成功") 
-      if(file1==""){
-        localStorage.setItem("file1",URI);
-      } 
-      else if(file2==""){
-        localStorage.setItem("file2",URI);
-      } 
-      else if(file3==""){
-        localStorage.setItem("file3",URI);
-      }    
-      console.log("emd");
-      file1 = localStorage.getItem("file1");
-      file2 = localStorage.getItem("file2");
-      file3 = localStorage.getItem("file3"); 
-      console.log(file1);
-      console.log(file2);
-      console.log(file3); 
+      let URL = `https://ipfs.infura.io/ipfs/${result.path}`;    
+      alert("上传成功") 
+      onFileUploaded(URL)
+      GetselectFile();
+      let showfile = sessionStorage.getItem("chooseuploadimg")
+      setShowFile(showfile)
   }
-
-  function  handledelete(event){  
-    if(Count==1){
-      setShowFile(localStorage.getItem("file2"))
-      var file2 = localStorage.getItem("file2");
-      var file3 = localStorage.getItem("file3");  
-      localStorage.setItem("file1",file2);
-      localStorage.setItem("file2",file3);
-    }
-    else if(Count ==2 ){
-      setShowFile(localStorage.getItem("file3"))
-      var file3 = localStorage.getItem("file3"); 
-      localStorage.setItem("file2",file3);
-      localStorage.setItem("file3","");
-      SetCount(2)
-    }
-    else{
-      setShowFile(localStorage.getItem("file2"))
-      localStorage.setItem("file3","");
-      SetCount(2)
-      } 
-      var file1 = localStorage.getItem("file1");
-      var file2 = localStorage.getItem("file2");
-      var file3 = localStorage.getItem("file3"); 
-      console.log(file1);
-      console.log(file2);
-      console.log(file3);
-  }
-  //tomgguoloucalstoragejiejue
-
-  function  handlecross(event){  
-    if(Count==1){
-      setShowFile( localStorage.getItem("file1"))
-    }
-    else if(Count ==2 ){
-      setShowFile(localStorage.getItem("file1"))
-    }
-    else{
-      setShowFile(localStorage.getItem("file2"))
-    }
-    SetCount(Count => Count>1?Count-1:Count)
-  }
-
-  function  handleadd(event){  
-    if(Count==1){
-      setShowFile( localStorage.getItem("file2"))
-    }
-    else if(Count==2||Count ==3 ){
-      setShowFile( localStorage.getItem("file3"))
-    }
-    SetCount(Count => Count<3?Count+1:Count)    
-  }
-
 
   const onDrop = useCallback (acceptedFiles =>  {
     const file = acceptedFiles[0];
-
     var reader = new window.FileReader();
     reader.readAsArrayBuffer(file);
     reader.onload = function(e){
@@ -114,10 +42,8 @@ const DropZone = ({ onFileUploaded })  => {
       buffer = reader.result;
       gen();
      } 
+   
     const fileUrl = URL.createObjectURL(file);
-    console.log(fileUrl)
-    setShowFile(fileUrl)
-    onFileUploaded(file);
   }, [onFileUploaded]);
 
 
@@ -125,21 +51,52 @@ const DropZone = ({ onFileUploaded })  => {
     onDrop, 
     accept: 'image/*'
   });
-  
+
+  function  HandleBack(event){  
+    console.log("desc")
+    Back()
+    let showfile = sessionStorage.getItem("chooseuploadimg")
+    setShowFile(showfile)
+  }
+
+  function  HandleDelete(event){  
+    console.log("desc")
+    Delete()
+    let showfile = sessionStorage.getItem("chooseuploadimg")
+    setShowFile(showfile)
+  }
+
+  function  HandleNext(event){  
+    console.log("desc")
+    Next()
+    let showfile = sessionStorage.getItem("chooseuploadimg")
+    setShowFile(showfile)
+  }
+    
   return (
-    <div className={classes.dropzone} {...getRootProps()}>
-      <input {...getInputProps()} accept='image/*' />
+    <div>
+        <div className={classes.dropzone} {...getRootProps()}>
+        { showFileRef.current=="-1" 
+            ? <input {...getInputProps()} accept='image/*' />
+            : <div></div>
+          }
+          
 
-      { showFile 
-        ? <img src={showFile} alt="Point thumbnail"/>
-        : (
-          <p>
-            <CloudUploadIcon />
-            Task Source
-          </p>
-        )
-      }
-
+          { showFileRef.current!="-1" 
+            ? <img src={showFileRef.current} alt="Point thumbnail"/>
+            : (
+              <p>
+                <CloudUploadIcon />
+                Task Source
+              </p>
+            )
+          }
+        
+        </div>
+        <Button onClick={HandleBack}>Back</Button>
+        { showFileRef.current!="-1" ? <Button onClick={HandleDelete}>Delete</Button>:
+         <Button disabled>Delete</Button>}
+        <Button onClick={HandleNext}>Next</Button>
     </div>
   );
 }
